39a208e269b9cd945ec09ef4b7bdc36796b18d19
Azraelix316 <jaredj13310@gmail.com>
2026-09-22 15:58:13 +0800

    remove unneccesary loop

--- src/main.cpp
+++ src/main.cpp
@@ -128,20 +128,6 @@ void opcontrol()
         crimson_cam.update();
         update_dashboard();
 
-        // Intake controls (Hold R1 = in, Hold R2 = out)
-        if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1))
-        {
-            intake.move(127);
-        }
-        else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R2))
-        {
-            intake.move(-127);
-        }
-        else
-        {
-            intake.move(0);
-        }
-
         // Update elevator control
         subsystems::elevator_update();
 
