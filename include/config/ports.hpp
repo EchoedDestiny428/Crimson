4fbb34352ff6df2be1e4d02707e0bdfda227d3df
echoeddestiny428 <zacharyng428@gmail.com>
2026-09-25 15:02:03 +0800

    new ports

--- include/config/ports.hpp
+++ include/config/ports.hpp
@@ -4,19 +4,23 @@
 
 namespace ports {
 
-inline constexpr std::int8_t kIntake = 1;
-inline constexpr std::int8_t kVision = 5;
-inline constexpr std::int8_t kImu = 6;
+inline constexpr std::int8_t kIntake = 2;
+inline constexpr std::int8_t kVision = 16; // temp
+inline constexpr std::int8_t kImu = 16; // temp
 
-inline constexpr std::int8_t kLeftDriveA = 3;
-inline constexpr std::int8_t kLeftDriveB = -20;
-inline constexpr std::int8_t kRightDriveA = -2;
-inline constexpr std::int8_t kRightDriveB = 19;
+inline constexpr std::int8_t kLeftDriveA = 1;
+inline constexpr std::int8_t kLeftDriveB = -18;
+inline constexpr std::int8_t kRightDriveA = 19;
+inline constexpr std::int8_t kRightDriveB = -3;
 
 inline constexpr std::int8_t kElevatorA = 4;
-inline constexpr std::int8_t kElevatorB = -18;
+inline constexpr std::int8_t kElevatorB = -20;
 
-inline constexpr std::int8_t kPivot = 11;
-inline constexpr std::int8_t kClaw = 12;
+inline constexpr std::int8_t kPivot = 5;
+inline constexpr std::int8_t kClaw = 8;
+
+
+//17 vert
+//21 horz
 
 }
